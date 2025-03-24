ef5155dc04c3ca6baaa5d961e62fc1b05f7582e4
ABHAYRAJSINGH11 <abhayraj28334@gmail.com>
2025-03-24 20:05:43 +0000

    For each Operation

---
+++ 03_itterations/ForEachOperation.js
@@ -0,0 +1,41 @@
+//ForEach Operation: Itterates for each value or item
+
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+coding.forEach( function (val){
+    console.log(val);
+} )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {     //we can retrive item, index and array from for each loop by this syntax
+//     console.log(item, index, arr);
+// } )
+
+// const myCoding = [
+//     {
+//         languageName: "javascript",
+//         languageFileName: "js"
+//     },
+//     {
+//         languageName: "java",
+//         languageFileName: "java"
+//     },
+//     {
+//         languageName: "python",
+//         languageFileName: "py"
+//     },
+// ]
+
+// myCoding.forEach( (item) => {
+    
+//     console.log(item.languageName);
+// } )
